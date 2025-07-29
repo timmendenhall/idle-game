ab8e61f7af61e70de83cdf3b528f3cb1f767f8e1
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-07-29 10:33:14 -0400

    wip

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -5,16 +5,32 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { PiBone } from 'react-icons/pi';
 import { useGameState } from '@/state/hooks';
 
+interface ResourceDisplayProps {
+    value: number;
+    children: React.ReactElement;
+}
+
+const ResourceDisplay = ({ value, children }: ResourceDisplayProps) => {
+    return (
+        <div className="flex flex-row items-center pl-3">
+            {children}
+            <span className="pl-1">{value.toFixed(2)}</span>
+        </div>
+    );
+};
+
 export const TopBar = () => {
     const { bones } = useGameState();
 
     return (
         <div className="sticky top-0 flex h-8 w-full flex-row items-center bg-neutral-800 p-1">
             <GiHamburgerMenu />
-            <div className="flex flex-row items-center pl-3">
+            <ResourceDisplay value={bones}>
                 <PiBone />
-                <span>{bones.toFixed(2)}</span>
-            </div>
+            </ResourceDisplay>
+            <ResourceDisplay value={bones}>
+                <PiBone />
+            </ResourceDisplay>
         </div>
     );
 };

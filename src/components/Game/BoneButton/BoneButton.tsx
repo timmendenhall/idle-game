c9790360d6e30ef6fe6b890db0de6b02e415e7fc
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-07-29 21:04:01 -0400

    wip bone button

--- src/components/Game/BoneButton/BoneButton.tsx
+++ src/components/Game/BoneButton/BoneButton.tsx
@@ -1,10 +1,20 @@
+'use client';
+
 import React from 'react';
 import { PiBone } from 'react-icons/pi';
 import { Button } from '@/components/ui';
+import { useGameStateDispatch } from '@/state/hooks';
+import { addBones } from '@/state/actions';
 
 export const BoneButton = () => {
+    const dispatch = useGameStateDispatch();
+    const handleOnClick = (): void => {
+        // TODO: calc add bones
+        dispatch(addBones(1));
+    };
+
     return (
-        <Button>
+        <Button onClick={handleOnClick}>
             <div className="pr-3">
                 <PiBone />
             </div>

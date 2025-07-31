650f285bd30480b4589e4a72310153b0a4a1f715
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-07-31 13:50:14 -0400

    Adding storybook but using older nextjs preset versus nextjs-vite (broken)

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from '@storybook/nextjs-vite';
+
+const config: StorybookConfig = {
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@chromatic-com/storybook',
+        '@storybook/addon-docs',
+        '@storybook/addon-a11y',
+        '@storybook/addon-vitest',
+    ],
+    framework: {
+        name: '@storybook/nextjs',
+        options: {},
+    },
+};
+export default config;

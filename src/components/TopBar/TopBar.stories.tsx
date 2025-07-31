8827556f96db8e6d621093cda52bb9742b6fcfff
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-07-31 18:59:08 -0400

    Stories for Button

--- src/components/TopBar/TopBar.stories.tsx
+++ src/components/TopBar/TopBar.stories.tsx
@@ -6,13 +6,10 @@ import { withGameState } from '@/../.storybook/decorators/GameStateDecorator';
 const meta = {
     component: TopBar,
     title: 'TopBar',
-    args: {},
     decorators: [withGameState({ bones: 42 })],
 } satisfies Meta<typeof TopBar>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-    args: {},
-};
+export const Default: Story = {};

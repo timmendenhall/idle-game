dc768d4f173c15251ac7574efa297e71a8012c92
Tim Mendenhall <5558561+timmendenhall@users.noreply.github.com>
2025-08-11 20:08:08 -0400

    Test updates

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,14 +1,55 @@
-import { expect, test, vi } from 'vitest';
+import { expect, describe, vi, beforeEach, it } from 'vitest';
 import { screen } from '@testing-library/react';
 import { render } from '@/test/util';
+import { prisma } from '@/prisma';
 
 import Page from './page';
 
 vi.mock('next/navigation');
 
-test('should render correctly', () => {
-    render(<Page />);
-    expect(
-        screen.getByRole('heading', { level: 1, name: 'Idle Game' }),
-    ).toBeDefined();
+vi.mock('@/prisma', () => ({
+    prisma: {
+        post: {
+            findMany: vi.fn(),
+        },
+    },
+}));
+
+describe('Home page', () => {
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it('fetches posts ordered by createdAt desc', async () => {
+        const mockPosts = [
+            { id: 1, title: 'First post' },
+            { id: 2, title: 'Second post' },
+        ];
+        // @ts-expect-error bad model data
+        vi.mocked(prisma.post.findMany).mockResolvedValue(mockPosts);
+
+        // since Home is async, await it
+        const Home = await Page();
+
+        render(Home);
+
+        expect(
+            screen.getByRole('heading', { level: 1, name: 'Idle Game' }),
+        ).toBeDefined();
+        expect(prisma.post.findMany).toHaveBeenCalledWith({
+            orderBy: { createdAt: 'desc' },
+        });
+        expect(screen.getByText(mockPosts[0].title)).toBeInTheDocument();
+        expect(screen.getByText(mockPosts[1].title)).toBeInTheDocument();
+    });
+
+    it('renders with no posts if database returns empty array', async () => {
+        vi.mocked(prisma.post.findMany).mockResolvedValue([]);
+
+        const Home = await Page();
+
+        render(Home);
+
+        expect(screen.queryByText('First post')).not.toBeInTheDocument();
+    });
 });
